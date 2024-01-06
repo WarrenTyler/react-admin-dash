ebe258849b49081428b00e145fd8b5b771568957
WarrenT <warrentyler1024@gmail.com>
2024-01-06 11:39:49 +0000

    Implement Topbar and integrate into App
    - Create Topbar component with search bar and action icons
    - Integrate Topbar into App layout
    - Utilize useContext for theme toggling
    - Import LightModeOutlinedIcon and DarkModeOutlinedIcon for dynamic color mode switch
    - Include NotificationsOutlinedIcon, SettingsOutlinedIcon, and PersonOutlinedIcon for additional actions
    - Enhance UI with theming based on the current palette mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar";
 
 const App = () => {
   const [theme, colorMode] = useMode();
@@ -9,7 +10,7 @@ const App = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content">main</main>
+          <main className="content"><Topbar /></main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
